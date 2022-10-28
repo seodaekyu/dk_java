8639ebc75288e373553cde72cba1548aef75925f
seodaekyu <grarlove7@naver.com>
2022-10-28 17:46:17 +0900

    Step10_interface 프로젝트 추가함

--- Step10_Interface/src/test/main/MainClass04.java
+++ Step10_Interface/src/test/main/MainClass04.java
@@ -15,7 +15,7 @@ public class MainClass04 {
 			System.out.println("천장에 구멍을 뚫어요");
 		}); // 메소드가 오버라이드가 1개 일 경우에만, 위에 방법을 밑의 방법으로도 쓸 수 있음
 		
-		ArrayList
+		
 		
 	}
 	// 1개의 추상 메소드로 구성되어 있는 Drill 객체를 인자로 전달받는 메소드
